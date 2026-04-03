388b5f19918042ec0b81904e52e05a1460d5cbaf
jbunds <jeffbunds@protonmail.ch>
2026-04-03 21:27:21 +0200

    refactor render.js by moving the custom cursor creation code and the listener registration code into helpers.js

--- demo/helpers.js
+++ demo/helpers.js
@@ -21,3 +21,48 @@ export async function launchChrome(url = URL) {
   };
   return await puppeteer.launch(options);
 }
+
+export async function createMousePointer(page) {
+  await page.evaluate((initialX = 0, initialY = 0) => {
+    const cursor = document.createElement('div');
+    cursor.id        = 'custom-pointer';
+    cursor.innerHTML = `<svg width="32" height="32" viewBox="0 0 32 32"><path d="M10 7v11.188l2.53-2.442 2.901 5.254 1.765-.941-2.775-5.202h3.604L10 7z" fill="black" stroke="white" stroke-width="1.5"/></svg>`;
+    Object.assign(cursor.style, {
+      position: 'fixed', top: '0', left: '0', width: '32px', height: '32px',
+      zIndex: '100', pointerEvents: 'none', transform: 'translate(0,0)'
+    });
+    document.documentElement.appendChild(cursor);
+    window.mouseX = initialX;
+    window.mouseY = initialY;
+    const updatePos = (e) => { window.mouseX = e.clientX; window.mouseY = e.clientY }
+    window.addEventListener('scroll',    updatePos);
+    window.addEventListener('mousemove', updatePos);
+    window.addEventListener('mousedown', () => { cursor.querySelector('path').style.fill = 'gray'  });
+    window.addEventListener('mouseup',   () => { cursor.querySelector('path').style.fill = 'black' });
+    function track() { // animation loop: updates at 60fps to follow the GhostCursor cursor's path
+      cursor.style.transform = `translate3d(${window.mouseX}px, ${window.mouseY}px, 0)`;
+      requestAnimationFrame(track);
+    }
+    track();
+  }, VIEWPORT.width  / 2, VIEWPORT.height / 2);
+}
+
+// register event listeners to track GhostCursor's cursor movements
+export async function registerListeners(page) {
+  const frames = page.frames();
+  for (const f of frames) {
+    if (f === page.mainFrame()) continue; // skip the main frame (handled above)
+    await f.evaluate(() => {
+      window.addEventListener('scroll', (e) => {
+        const rect = window.frameElement.getBoundingClientRect();
+        window.parent.mouseX = e.clientX + rect.left;
+        window.parent.mouseY = e.clientY + rect.top;
+      });
+      window.addEventListener('mousemove', (e) => {
+        const rect = window.frameElement.getBoundingClientRect();
+        window.parent.mouseX = e.clientX + rect.left;
+        window.parent.mouseY = e.clientY + rect.top;
+      });
+    });
+  }
+}
--- demo/render.js
+++ demo/render.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
-import { URL, VIEWPORT, launchChrome } from './helpers.js';
 import { GhostCursor, installMouseHelper } from 'ghost-cursor';
+import { URL, VIEWPORT, launchChrome, createMousePointer, registerListeners } from './helpers.js';
 
 const OUTPUT = 'demo.webm';
 
@@ -8,47 +8,9 @@ const OUTPUT = 'demo.webm';
   const browser = await launchChrome(URL);
   const [page]  = await browser.pages();
 
-  // create SVG mouse pointer
-  await page.evaluate((initialX = 0, initialY = 0) => {
-    const cursor = document.createElement('div');
-    cursor.id        = 'manual-pointer';
-    cursor.innerHTML = `<svg width="32" height="32" viewBox="0 0 32 32"><path d="M10 7v11.188l2.53-2.442 2.901 5.254 1.765-.941-2.775-5.202h3.604L10 7z" fill="black" stroke="white" stroke-width="1.5"/></svg>`;
-    Object.assign(cursor.style, {
-      position: 'fixed', top: '0', left: '0', width: '32px', height: '32px',
-      zIndex: '100', pointerEvents: 'none', transform: 'translate(0,0)'
-    });
-    document.documentElement.appendChild(cursor);
-    window.mouseX = initialX;
-    window.mouseY = initialY;
-    const updatePos = (e) => { window.mouseX = e.clientX; window.mouseY = e.clientY }
-    window.addEventListener('scroll',    updatePos);
-    window.addEventListener('mousemove', updatePos);
-    window.addEventListener('mousedown', () => { cursor.querySelector('path').style.fill = 'gray'  });
-    window.addEventListener('mouseup',   () => { cursor.querySelector('path').style.fill = 'black' });
-    function track() { // animation loop: updates at 60fps to follow the GhostCursor cursor's path
-      cursor.style.transform = `translate3d(${window.mouseX}px, ${window.mouseY}px, 0)`;
-      requestAnimationFrame(track);
-    }
-    track();
-  }, VIEWPORT.width  / 2, VIEWPORT.height / 2);
+  await createMousePointer(page);
 
-  // register event listeners to track GhostCursor's cursor movements
-  const frames = page.frames();
-  for (const f of frames) {
-    if (f === page.mainFrame()) continue; // skip the main frame (handled above)
-    await f.evaluate(() => {
-      window.addEventListener('scroll', (e) => {
-        const rect = window.frameElement.getBoundingClientRect();
-        window.parent.mouseX = e.clientX + rect.left;
-        window.parent.mouseY = e.clientY + rect.top;
-      });
-      window.addEventListener('mousemove', (e) => {
-        const rect = window.frameElement.getBoundingClientRect();
-        window.parent.mouseX = e.clientX + rect.left;
-        window.parent.mouseY = e.clientY + rect.top;
-      });
-    });
-  }
+  await registerListeners(page);
 
   const cursor = new GhostCursor(page, {
     start: { x: VIEWPORT.width  / 2, y: VIEWPORT.height / 2 },
