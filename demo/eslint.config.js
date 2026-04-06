4e13bfee22eecba5fa3c114c1e205c5af36f445d
jbunds <jeffbunds@protonmail.ch>
2026-04-06 18:05:06 +0200

    simplify eslint config

--- demo/eslint.config.js
+++ demo/eslint.config.js
@@ -1,11 +1,10 @@
 import globals          from 'globals';
 import js               from '@eslint/js';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default [
   js.configs.recommended, {
     languageOptions: {
       globals: { ...globals.browser },
     },
   }
-]);
+];
