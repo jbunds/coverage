3a1ad9ec7b1c45cb83999c3d7bdbea97702bc29e
jbunds <jeffbunds@protonmail.ch>
2026-04-06 18:00:26 +0200

    remove dependency upon eslint-plugin-file-progress

--- demo/eslint.config.js
+++ demo/eslint.config.js
@@ -1,14 +1,9 @@
 import globals          from 'globals';
 import js               from '@eslint/js';
-import progress         from 'eslint-plugin-file-progress';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  js.configs.recommended,
-  progress.configs.recommended, {
-    plugins: {
-      progress,
-    },
+  js.configs.recommended, {
     languageOptions: {
       globals: { ...globals.browser },
     },
