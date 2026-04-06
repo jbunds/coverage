57bed73a93f8af12c3b0c85368cbdd70987c9215
jbunds <jeffbunds@protonmail.ch>
2026-04-06 17:02:06 +0200

    show eslint progress during CI-triggered ESLint runs

--- demo/eslint.config.js
+++ demo/eslint.config.js
@@ -1,9 +1,14 @@
 import globals          from 'globals';
 import js               from '@eslint/js';
+import progress         from 'eslint-plugin-file-progress';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  js.configs.recommended, {
+  js.configs.recommended,
+  progress.configs.recommended, {
+    plugins: {
+      progress,
+    },
     languageOptions: {
       globals: { ...globals.browser },
     },
