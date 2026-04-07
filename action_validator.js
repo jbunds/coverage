e938ffe74c1da1d55828ecb36c1e61be05fa6dce
jbunds <jeffbunds@protonmail.ch>
2026-04-07 13:41:47 +0200

    update action_validator.js (rename immediately forthcoming) to validate all actions and workflows

--- action_validator.js
+++ action_validator.js
@@ -1,10 +1,39 @@
-import { readFileSync   } from 'fs';
-import { validateAction } from '@action-validator/core';
+import { readFileSync                     } from 'fs'
+import { validateAction, validateWorkflow } from '@action-validator/core'
 
-const actionSource = readFileSync('./action.yml', 'utf8');
-const state        = validateAction(actionSource);
+const actionFiles = [
+  './action.yml',
+  '.github/actions/setup-node/action.yml',
+  '.github/actions/upload-pages/action.yml'
+]
 
-if (state.errors.length > 0) {
-  console.error('validation failed:', state.errors);
-  process.exit(1);
+const workflowFiles = [
+  '.github/workflows/lint-css.yml',
+  '.github/workflows/lint-go.yml',
+  '.github/workflows/lint-js.yml',
+  '.github/workflows/pages.yml',
+  '.github/workflows/test-go.yml',
+  '.github/workflows/validate-action.yml',
+]
+
+for (const actionFile of actionFiles) {
+  const state = validateAction(readFileSync(actionFile, 'utf8'))
+
+  if (state.errors.length > 0) {
+    console.error(`${actionFile} is invalid:`, state.errors)
+    process.exit(1)
+  } else {
+    console.log(`${actionFile} is valid`)
+  }
+}
+
+for (const workflowFile of workflowFiles) {
+  const state = validateWorkflow(readFileSync(workflowFile, 'utf8'))
+
+  if (state.errors.length > 0) {
+    console.error(`${workflowFile} is invalid:`, state.errors)
+    process.exit(1)
+  } else {
+    console.log(`${workflowFile} is valid`)
+  }
 }
