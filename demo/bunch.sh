0b9023dda3c7900ec28842521b583c75b12cd846
jbunds <jeffbunds@protonmail.ch>
2026-04-19 15:52:56 +0200

    add a comment to the bunch.sh script outlining its purpose

--- demo/bunch.sh
+++ demo/bunch.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# this script runs the render.js script 20 times to create 20 numbered
+# demo WebM files as candidates for a good demo recording
+
 for i in {1..20}; do
   fname=$(printf '%02d\n' $i)
   node render.js
