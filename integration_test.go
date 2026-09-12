09c018ea22063aa3133a10c09d38bb7e7c875494
jbunds <jeffbunds@protonmail.ch>
2026-09-12 15:59:15 +0200

    update testdata/cov.out to reflect the latest changes to non-test *.go files
    update testdata/main.go.html to reflect the latest changes committed to main.go
    
    update the TestIntegrationTest test to launch `meld` to view unexpected diffs
    
    define a custom reporter in integration_test.go that doesn't truncate strings

--- integration_test.go
+++ integration_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -13,6 +15,7 @@ import (
 
 func TestIntegrationTest(t *testing.T) {
 	t.Parallel()
+
 	tmpDir        := t.TempDir()
 	profiles, err := cover.ParseProfiles("testdata/cov.out"); if err != nil { t.Fatal(err) }
 	rg            := &reportGenerator{
@@ -23,40 +26,61 @@ func TestIntegrationTest(t *testing.T) {
 
 	if err := rg.getModName(t.Context(), "go.mod"); err != nil { t.Fatal(err) }
 
-	// hack to avoid calling writeStyleCSS which requires rg.embeddedFiles
-	// the style.css file is copied purely as a convenience measure to aid in debugging CSS rendering errors
-	if err := copyFile("css/style.css", filepath.Join(tmpDir, "style.css")); err != nil { t.Fatal(err) }
-
-	if err := rg.writeCovHTMLFiles(t.Context(), io.Discard,   "style.css");  err != nil { t.Fatal(err) }
+	if err := rg.writeCovHTMLFiles(t.Context(), io.Discard, "style.css");  err != nil { t.Fatal(err) }
 
 	tests := []struct{
 		name string
 	}{
 		{name: "flags.go"},
-//		{name: "main.go"},
+		{name: "main.go"},
 		{name: "tree.go"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
+
 			goldenFile := tt.name + ".html"
-			want, err  := os.ReadFile(filepath.Join("testdata",         goldenFile)); if err != nil { t.Fatal(err) } // #nosec G304
-			got,  err  := os.ReadFile(filepath.Join(tmpDir, rg.modName, goldenFile)); if err != nil { t.Fatal(err) } // #nosec G304
+
+			wantPath   := filepath.Join("testdata",         goldenFile)
+			gotPath    := filepath.Join(tmpDir, rg.modName, goldenFile)
+
+			want, err  := os.ReadFile(wantPath); if err != nil { t.Fatal(err) } // #nosec G304
+			got,  err  := os.ReadFile( gotPath); if err != nil { t.Fatal(err) } // #nosec G304
+
 			wantLines  := strings.Split(strings.TrimSuffix(string(want), "\n"), "\n")
 			gotLines   := strings.Split(strings.TrimSuffix(string( got), "\n"), "\n")
-			if diff := cmp.Diff(wantLines, gotLines); diff != "" {
-				t.Errorf("mismatch (-want +got):\n%s", diff)
+
+			if !cmp.Equal(wantLines, gotLines) {
+				if os.Getenv("GITHUB_ACTIONS") != "true" && // https://docs.github.com/actions/reference/workflows-and-actions/variables
+				   os.Getenv("CI"            ) != "true" {
+					cmd := exec.Command("meld", wantPath, gotPath) // #nosec G204
+					_ = cmd.Run()
+				}
+				var rep reporter
+				cmp.Equal(wantLines, gotLines, cmp.Reporter(&rep))
+				t.Errorf("mismatch (-want +got):\n%s", strings.Join(rep.diffs, "\n"))
 			}
 		})
 	}
 }
 
-func copyFile(src, dst string) error {
-	in,  err := os.Open(src);     if err != nil { return err } // #nosec G304
-	defer in.Close()
-	out, err := os.Create(dst);   if err != nil { return err } // #nosec G304
-	defer out.Close()
-	_, err    = io.Copy(out, in); if err != nil { return err }
-	return out.Sync()
+type reporter struct{
+	path  cmp.Path
+	diffs []string
+}
+
+func (r *reporter) PushStep(ps cmp.PathStep) {
+	r.path = append(r.path, ps)
+}
+
+func (r *reporter) PopStep() {
+	r.path = r.path[:len(r.path) - 1]
+}
+
+func (r *reporter) Report(rs cmp.Result) {
+	if !rs.Equal() {
+		want, got := r.path.Last().Values() 
+			r.diffs = append(r.diffs, fmt.Sprintf("- %s\n+ %s", want, got))
+	}
 }
